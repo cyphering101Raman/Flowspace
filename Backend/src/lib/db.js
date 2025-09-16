c0a9ede68178f649b3b68ef9810de2baa575a18e
cyphering101Raman <guptaraman600@gmail.com>
2025-09-16 18:14:12 +0530

    Add mongoose connection

---
+++ Backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+    console.log(`✅ MongoDB Connected.`);
+    console.log(`DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log(`❌ MongoDB Connection Failed: ${error}`);
+  }
+}
+
+export default connectDB;
