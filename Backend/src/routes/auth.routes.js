24ce2f331fbe0ebb61fdc62163ea9350f9642aaa
cyphering101Raman <guptaraman600@gmail.com>
2025-09-16 18:16:00 +0530

    add user routes

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+import { signup, login, logout, checkAuth } from "../controllers/auth.controller.js";
+import protectedRoute from "../middleware/auth.middleware.js";
+
+const route = Router()
+
+route.post("/signup", signup)
+route.post("/login", login)
+route.post("/logout", logout)
+
+route.get("/check", protectedRoute, checkAuth)
+
+export default route;
