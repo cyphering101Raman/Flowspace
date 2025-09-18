feac360d736f501d4f7b5141c7a42492c32b63ec
cyphering101Raman <guptaraman600@gmail.com>
2025-09-18 18:26:01 +0530

    feat: add Document route for viewing documents by ID

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -4,13 +4,17 @@ import App from "../App.jsx";
 import Signup from "../pages/Signup.jsx"
 import Login from "../pages/Login.jsx";
 import Home from "../pages/Home.jsx";
+import Document from "../pages/Document.jsx";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index element={<Home/>}></Route>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />}></Route>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/document" element={<Document />}>
+        <Route path=":id" element={<Document />} />
+      </Route>
     </Route>
   )
 )
